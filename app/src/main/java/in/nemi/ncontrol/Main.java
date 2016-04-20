9ec9a77a7c51d1ba2f0bd120cc6e0f646a89db55
Shourya Sharma <shouryasharma@nemi.in>
2016-04-21 00:22:14 +0530

    initial
    initial commit

---
+++ app/src/main/java/in/nemi/ncontrol/Main.java
@@ -0,0 +1,15 @@
+package in.nemi.ncontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shouryas on 4/21/2016.
+ */
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
