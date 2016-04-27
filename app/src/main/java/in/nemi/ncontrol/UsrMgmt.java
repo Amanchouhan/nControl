b04b27f5df4a0a112fdb5fab92a120e7738782d5
Shourya Sharma <shouryasharma@nemi.in>
2016-04-27 23:16:33 +0530

    New layout for usrmgmt.xml

--- app/src/main/java/in/nemi/ncontrol/UsrMgmt.java
+++ app/src/main/java/in/nemi/ncontrol/UsrMgmt.java
@@ -1,39 +1,35 @@
 package in.nemi.ncontrol;
 
 import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.CursorAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Developer on 25-04-2016.
  */
-public class UsrMgmt extends Activity {
-
+public class UsrMgmt extends Activity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.usrmgmt);
-
-        ndbHelper databaseHelper;
-        databaseHelper = new ndbHelper(this, null, null, 1);
-        Cursor cursor = databaseHelper.getUsers();
-        String[] users = new String[]{
-                databaseHelper.COLUMN_USERNAME
-        };
-        int[] id = new int[]{};
-
-        ListAdapter userAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, users);
-        ListView userListView = (ListView) findViewById(R.id.userListView);
-        userListView.setAdapter(userAdapter);
-
     }
 }
